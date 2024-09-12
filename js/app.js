5668443c75d5e936df1abd5403bea81b3893004a
Dize dev <axiozweb2022@gmail.com>
2024-09-11 21:48:44 -0500

    funcionalidad para configurar la alarma

--- js/app.js
+++ js/app.js
@@ -7,6 +7,18 @@ const Elementclock = {
     pm: document.querySelector('.pm'),
 };
 
+const ringAlarm = () => {
+    alarmRing = true;
+    const audioAlarm = new Audio('./assets/alarm.mp3');
+    audioAlarm.play;
+
+    audioAlarm.addEventListener('ended', () => {
+        alarmRing = false;
+    });
+};
+
+let alarmRing = false;
+
 const setTime = ({ secondHand, minuteHand, hourHand, am, pm }) => {
     const now = new Date();
     const today = now.getDay();
@@ -30,7 +42,153 @@ const setTime = ({ secondHand, minuteHand, hourHand, am, pm }) => {
         am.classList.add('active');
         pm.classList.remove('active');
     }
+
+    let minutesString = minutes.toString();
+    if (minutesString.length < 2) {
+        minutesString = `0  + ${minutesString}`;
+    }
+    let hourString = hours.toString();
+    if (hourString.length < 2) {
+        hourString = `0  + ${hourString}`;
+    }
+
+    alarms.map(({ days, time, active }) => {
+        let dayMacht = false;
+
+        days.map(day => {
+            if (day === today) {
+                dayMacht = true;
+            }
+        });
+
+        if (
+            time === `${hourString}:${minutesString}` &&
+            active &&
+            dayMacht &&
+            alarmRing === false
+        )
+            ringAlarm();
+    });
+
+    const tick = new Audio('./assets/tick.mp3');
+    tick.play;
 };
+
+const alarms = [
+    {
+        id: 1,
+        time: '9:47',
+        name: 'Wake up',
+        active: true,
+        days: [1, 2, 3, 4, 5, 6],
+    },
+    {
+        id: 2,
+        time: '12:00',
+        name: 'Wake up',
+        active: false,
+        days: [1, 2, 3, 4, 5, 6],
+    },
+    {
+        id: 3,
+        time: '12:00',
+        name: 'Wake up',
+        active: false,
+        days: [1, 2, 3, 4, 5, 6],
+    },
+    {
+        id: 4,
+        time: '12:00',
+        name: 'Wake up',
+        active: false,
+        days: [1, 2, 3, 4, 5],
+    },
+    {
+        id: 5,
+        time: '12:00',
+        name: 'Wake up',
+        active: false,
+        days: [1, 2, 3, 4, 5],
+    },
+    {
+        id: 6,
+        time: '12:00',
+        name: 'Wake up',
+        active: false,
+        days: [1, 2, 3, 4, 5],
+    },
+    {
+        id: 7,
+        time: '12:00',
+        name: 'Wake up',
+        active: false,
+        days: [1, 2, 3, 4, 5],
+    },
+];
+
+const alarmList = document.querySelector('.alarms');
+
+const dayName = day => ['L', 'M', 'M', 'J', 'V', 'S', 'D'][day];
+
+const initAlarm = () => {
+    if (!alarms.length)
+        alarmList.innerHTML = `<div class="no-alarms">No Alarms, click on below + button to  add one </div> `;
+
+    alarms.map(({ days, id, name, time, active }) => {
+        const alarmElement = document.createElement('div');
+        alarmElement.classList.add('alarm');
+        alarmElement.dataset.id = id;
+
+        let alarmDays = '';
+
+        days.map(day => {
+            const dayeLeater = dayName(day);
+            alarmDays += `<div class="day">${dayeLeater}</div>`;
+        });
+
+        const activeAAlarm = active ? 'checked' : '';
+        alarmElement.innerHTML = ` 
+               <div class="head">
+                    <div class="name">${name}</div>
+                </div>
+                <div class="body">
+                    <div class="left">
+                        <div class="time">${time}</div>
+                         <label class="toggle">
+                            <input type="checkbox" class="checkbox" hidden ${activeAAlarm} />
+                            <div class="track"></div>
+                            <div class="thumb"></div>
+                        </label>
+                    </div>
+
+                    <div class="days">${alarmDays}</div>
+                </div>
+                <div class="delete">
+                    <i class="ri-delete-bin-4-line icon--delete"></i>
+                </div> 
+        `;
+        alarmList.appendChild(alarmElement);
+    });
+};
+// funcionality to delete allarm or active inactive
+
+alarmList.addEventListener('click', event => {
+    if (event.target.closest('.checkbox')) {
+        const alarmID = parseInt(event.target.closest('.alarm').dataset.id);
+        const alarm = alarms.find(alarm => alarm.id === alarmID);
+
+        alarm.active = !alarm.active;
+    }
+    if (event.target.closest('.delete')) {
+        const alarmID = parseInt(event.target.closest('.alarm').dataset.id);
+        const alarm = alarms.find(alarm => alarm.id === alarmID);
+
+        alarms.splice(alarms.indexOf(alarm), 1);
+        alarmList.removeChild(event.target.closest('.alarm'));
+    }
+});
+
+initAlarm();
 
 const initClock = ({ clock }) => {
     const numbers = clock.querySelector('.numbers');
